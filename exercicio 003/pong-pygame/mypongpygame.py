afad0083268e820d9e44b9923f651f83229d0ed3
brunobrasbarros <bbb.snf24@uea.edu.br>
2024-09-09 11:14:50 -0400

    code alterations for a better gameplay

--- exercicio 003/pong-pygame/mypongpygame.py
+++ exercicio 003/pong-pygame/mypongpygame.py
@@ -24,19 +24,24 @@ pygame.display.set_caption("MyPong - PyGame Edition - 2024-09-02")
 
 # score text
 score_font = pygame.font.Font('assets/PressStart2P.ttf', 44)
-score_text = score_font.render('00 x 00', True, COLOR_WHITE, COLOR_BLACK)
+score_text = score_font.render('00 x 00', True,
+                               COLOR_WHITE, COLOR_BLACK)
 score_text_rect = score_text.get_rect()
 score_text_rect.center = (680, 50)
 
 # victory text
 victory_font = pygame.font.Font('assets/PressStart2P.ttf', 100)
-victory_text = victory_font.render('VICTORY', True, COLOR_WHITE, COLOR_BLACK)
+victory_text = victory_font.render('VICTORY', True,
+                                   COLOR_WHITE, COLOR_BLACK)
 victory_text_rect = score_text.get_rect()
 victory_text_rect.center = (450, 350)
 
+
+
 # sound effects
 bounce_sound_effect = pygame.mixer.Sound('assets/bounce.wav')
-scoring_sound_effect = pygame.mixer.Sound('assets/258020__kodack__arcade-bleep-sound.wav')
+scoring_sound_effect = (pygame.mixer.Sound
+                        ('assets/258020__kodack__arcade-bleep-sound.wav'))
 
 # player 1
 player_1 = pygame.image.load("assets/player.png")
@@ -54,8 +59,9 @@ ball_x = 640
 ball_y = 360
 ball_dx = 2.5
 ball_dy = 2.5
-ball_speed = 2.5
-initial_ball_speed = 2.5
+ball_speed = 2.2
+initial_ball_speed = 2.2
+
 
 # score
 score_1 = 0
@@ -69,9 +75,10 @@ game_clock = pygame.time.Clock()
 soundtrack_list =\
 ["SOUNDTRACKS/Mega Man 2 - Dr. Wily's Castle - UryaV.mp3",
 "SOUNDTRACKS/DuckTales Music (NES) - The Moon Theme - explod2A03.mp3",
-"SOUNDTRACKS/Masked Dedede 8 Bit Remix - Kirby Super Star Ultra (Konami VRC6) - Bulby.mp3",
-"SOUNDTRACKS/Castlevania II Music (NES) - Bloody Tears (Day Theme) - explod2A03.mp3",
-"SOUNDTRACKS/Undertale Ost_ 087 - Hopes and Dreams - Misaki.mp3"
+"SOUNDTRACKS/Masked Dedede 8 Bit Remix - Kirby Super Star Ultra (Konami VRC6) "
+"- Bulby.mp3",
+"SOUNDTRACKS/Castlevania II Music (NES) - Bloody Tears (Day Theme) - "
+"explod2A03.mp3"
  ]
 
 soundtrack_selection = random.choice(soundtrack_list)
@@ -106,61 +113,69 @@ while game_loop:
         screen.fill(COLOR_BLACK)
 
         # ball collision with the wall
-        if ball_y > 690:
+        if ball_y >=690:
+            ball_y = 690
             ball_dy *= -1
             bounce_sound_effect.play()
 
-        elif ball_y <= 10:
+        elif ball_y <= 0:
+            ball_y = 0
             ball_dy *= -1
             bounce_sound_effect.play()
 
         # ball collision with the player 1's paddle
-        if 100 > ball_x > 50:
-            if player_1_y < ball_y + 50 and player_1_y + 175 > ball_y:
-                ball_dx *= -1.0
+        if 25 <= ball_x <= 100:
+            if player_1_y <= ball_y <= player_1_y + 150:
+                if ball_dx <= 0:
+                    ball_dx *= - 1
 
-                # ball's impact point
-                impact_point = ball_y - player_1_y
+                    # ball's impact point
+                    impact_point = ball_y - player_1_y
 
-                offset = impact_point - 75  #  paddle's center
+                    offset = impact_point - 75  #  paddle's center
 
-                # angle and speed adjustment
-                ball_dy += offset * 0.02
+                    # angle and speed adjustment
+                    ball_dy += offset * 0.02
 
-                ball_speed = min(ball_speed + 0.2, 4)
+                    ball_speed = min(ball_speed + 0.2, 4)
 
-                bounce_sound_effect.play()
+                    ball_x = 101
+
+                    bounce_sound_effect.play()
 
         # ball collision with the player 2's paddle
-        if 1160 < ball_x < 1200:
-            if player_2_y < ball_y + 25 and player_2_y + 150 > ball_y:
-                ball_dx *= -1
+        if 1160 <= ball_x <= 1200:
+            if player_2_y <= ball_y <= player_2_y + 150:
+                if ball_dx >= 0:
+                    ball_dx *= -1
 
-                # ball's impact point
-                impact_point = ball_y - player_2_y
-                offset = impact_point - 75  # paddle's center
+                    # ball's impact point
+                    impact_point = ball_y - player_2_y
+                    offset = impact_point - 75  # paddle's center
 
-                # angle and speed adjustment
-                ball_dy += offset * 0.02
-                ball_speed = min(ball_speed + 0.2, 4)
-                bounce_sound_effect.play()
+                    # angle and speed adjustment
+                    ball_dy += offset * 0.02
+                    ball_speed = min(ball_speed + 0.2, 4)
+                    ball_x = 1159
+                    bounce_sound_effect.play()
 
         # scoring points
         if ball_x < 0:
             ball_x = 640
             ball_y = 360
-            ball_dy = 2.1
-            ball_dx = 2.1
-            ball_speed = 2.1
+            ball_dy = 2.5
+            ball_dx = 2.5
+            ball_speed = 2.2
             ball_speed = initial_ball_speed
             score_2 += 1
             scoring_sound_effect.play()
+
         elif ball_x > 1280:
             ball_x = 640
             ball_y = 360
-            ball_dy = 2.1
-            ball_dx = 2.1
-            ball_speed = 2.1
+            ball_dy = 2.5
+            ball_dx = 2.5
+            ball_speed = 2.2
             ball_speed = initial_ball_speed
             score_1 += 1
             scoring_sound_effect.play()
@@ -170,9 +185,9 @@ while game_loop:
         ball_y += ball_dy * ball_speed
 
         # player 1 up movement
-        if player_1_move_up:
+        if player_1_move_up and player_1_y >0 :
             player_1_y -= 5
-        if player_1_move_down:
+        if player_1_move_down and player_1_y < 570:
             player_1_y += 5
 
         # player 1 collides with upper wall
@@ -185,10 +200,10 @@ while game_loop:
 
         # player 2 "Artificial Intelligence"
         if ball_x > 640:
-            if player_2_y + 75 < ball_y:
-                player_2_y += random.randint(4, 5)
+            if player_2_y + 75 <= ball_y:
+                player_2_y += 4.7
             elif player_2_y + 75 > ball_y:
-                player_2_y -= random.randint(4, 5)
+                player_2_y -= 4.7
 
         #player 2 paddle returning to the original place
         if player_2_y <= 0:
@@ -205,6 +220,7 @@ while game_loop:
         screen.blit(player_1, (50, player_1_y))
         screen.blit(player_2, (1180, player_2_y))
         screen.blit(score_text, score_text_rect)
+
 
     else:
         # drawing victory
